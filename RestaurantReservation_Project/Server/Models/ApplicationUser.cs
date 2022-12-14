b28206ed63d09a7845ce377ed184331f4176fa7f
JASON PHUA KAI XIANG 2105200F <2105200F@student.tp.edu.sg>
2022-12-14 11:04:04 +0800

    Added Login System & Identity Server

--- RestaurantReservation_Project/Server/Models/ApplicationUser.cs
+++ RestaurantReservation_Project/Server/Models/ApplicationUser.cs
@@ -8,5 +8,7 @@ namespace RestaurantReservation_Project.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
--- RestaurantReservation_Project/Server/Startup.cs
+++ RestaurantReservation_Project/Server/Startup.cs
@@ -32,7 +32,7 @@ namespace RestaurantReservation_Project.Server
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
