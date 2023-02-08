fa27d69e5043b7695df80b9938fc53f08f3fe30e
JASON PHUA KAI XIANG 2105200F <2105200F@student.tp.edu.sg>
2023-02-09 07:14:34 +0800

    Added Form validation migration, and view of all three entities -jason

--- RestaurantReservation_Project/Shared/Domain/AppUser.cs
+++ RestaurantReservation_Project/Shared/Domain/AppUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,18 @@ namespace RestaurantReservation_Project.Shared.Domain
     public class AppUser
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name does not meet length requirements")]
         public string Name { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Gender { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Email Address is not a valid email")]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"(6|8|9)\d{7}", ErrorMessage = "Contact is not a valid phone number")]
         public int Contact { get; set; }
         public string Address { get; set; }
         public virtual List<Reservation> Reservations { get; set; }
--- RestaurantReservation_Project/Shared/Domain/Reservation.cs
+++ RestaurantReservation_Project/Shared/Domain/Reservation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,10 @@ namespace RestaurantReservation_Project.Shared.Domain
     public class Reservation
     {
         public int Id { get; set; }
+        [Required]
         public int GroupSize { get; set; }
+        [Required]
+        [DataType(DataType.Date, ErrorMessage = "Date must be set")]
         public DateTime DateTime { get; set; }
         public virtual AppUser AppUser { get; set; }
         public int AppUserId { get; set; }
