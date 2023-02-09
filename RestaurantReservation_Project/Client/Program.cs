cbc5abc0c1a009902140ddd0d960ce4fcac985ad
JASON PHUA KAI XIANG 2105200F <2105200F@student.tp.edu.sg>
2023-02-09 08:25:49 +0800

    added global error handling and updated layout

--- RestaurantReservation_Project/Client/Program.cs
+++ RestaurantReservation_Project/Client/Program.cs
@@ -3,11 +3,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RestaurantReservation_Project.Client.Services;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 namespace RestaurantReservation_Project.Client
 {
@@ -18,12 +20,20 @@ namespace RestaurantReservation_Project.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddHttpClient("RestaurantReservation_Project.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+            //builder.Services.AddHttpClient("RestaurantReservation_Project.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+            //    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+            builder.Services.AddHttpClient("RestaurantReservation_Project.ServerAPI", (sp, client) =>
+            {
+                client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+                client.EnableIntercept(sp);
+            })
+            .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
             // Supply HttpClient instances that include access tokens when making requests to the server project
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("RestaurantReservation_Project.ServerAPI"));
 
+            builder.Services.AddHttpClientInterceptor();
+            builder.Services.AddScoped<HttpInterceptorService>();
             builder.Services.AddApiAuthorization();
 
             await builder.Build().RunAsync();
